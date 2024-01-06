0cf69884350473173188d6a67f94284704c560c8
Jordan Williams <129204588+JordansFamiliar@users.noreply.github.com>
2024-01-06 19:01:10 +0200

    Update Header.js

--- swift-react-app/src/components/Header.js
+++ swift-react-app/src/components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import './Header.css';
 
-function Header() {
+function Header({ isLoggedIn }) {
   return (
     <div className="header">
       <h1>SWIFTLY TASKS</h1>
-    </div>
+      {isLoggedIn ? (
+        <div className="notification-icon">
+          <img src="notification-icon.svg" alt="Notification" />
+          <span className="notification-counter">3</span>
+        </div>
+      ) : (
+        <div className="auth-buttons">
+          <Button variant="contained" color="primary">
+            Sign Up
+          </Button>
+        </div>
+       )}
+  </div>
   );
 }
 

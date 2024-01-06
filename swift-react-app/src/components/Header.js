886e13f86638d662195813ea8c931bccbfede86a
Jordan Williams <129204588+JordansFamiliar@users.noreply.github.com>
2024-01-06 17:15:13 +0200

    Update Header.js

---
+++ swift-react-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>SWIFTLY TASKS</h1>
+    </div>
+  );
+}
+
+export default Header;

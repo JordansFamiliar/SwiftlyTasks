b238913605defcac49c83fa6e39e5e3a3629d16e
Jordan <measlybean@gmail.com>
2024-01-29 10:34:05 +0200

    Added new database

--- Backend/mysite/mysite/settings.py
+++ Backend/mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +87,7 @@ WSGI_APPLICATION = 'mysite.wsgi.app'
 DATABASES = {
     'default': dj_database_url.config(
 	default=os.environ.get('ELEPHANTSQL_URL', 'postgres://rhsylrbp:K8KcHDAj0d7sZilF9WDy_uX5IqowADJN@kashin.db.elephantsql.com/rhsylrbp')
+	)
 }
 
 # Password validation

0b8a531b36424af2b8cd54d744add839523721c2
Jordan <measlybean@gmail.com>
2024-01-31 11:07:09 +0200

    Added console.log to view cookies

--- Frontend/swift-react-app/src/components/utils.js
+++ Frontend/swift-react-app/src/components/utils.js
@@ -2,6 +2,7 @@
 export const getCookie = (name) => {
   let cookieValue = null;
   if (document.cookie && document.cookie !== '') {
+    console.log("Cookies:", document.cookie);
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();

283145ac1d5866c8bba01aaed7266c1c9cc9d742
Jordan <measlybean@gmail.com>
2024-01-31 11:53:34 +0200

    reset flags2

--- Backend/mysite/mysite/settings.py
+++ Backend/mysite/mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['.vercel.app']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = False #'None'
-SESSION_COOKIE_SAMESITE = False #'None'
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://swiftly-tasks.netlify.app']
 

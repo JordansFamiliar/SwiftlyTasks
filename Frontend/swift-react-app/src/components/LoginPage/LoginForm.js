7ecc29472860fea6d8f099634ed44db4351891b0
Jordan <measlybean@gmail.com>
2024-02-01 16:21:42 +0200

    update

--- Frontend/swift-react-app/src/components/LoginPage/LoginForm.js
+++ Frontend/swift-react-app/src/components/LoginPage/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 //import { setCsrftoken } from '../../redux/csrftokenSlice';
 import { TextField, Button } from '@mui/material';
 //import { useDispatch, useSelector } from 'react-redux';
